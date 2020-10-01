af28aadc139cffa21803a5135642e1feec9542a1
Yasser Kassem <47756869+YasserKassem989979@users.noreply.github.com>
2020-10-01 17:22:18 +0300

    Update Interview.js

--- Interview.js
+++ Interview.js
@@ -58,3 +58,26 @@ var obj = {
   }
   }
 obj.method(fn,1)
+/////////////////////////////////////////////////////////////////////////
+refactor with hooks
+//////////////////////////////////////////
+class App extends React.Component {
+  state = {
+    value: localStorage.getItem("info") || ""
+  };
+  componentDidUpdate() {
+    localStorage.setItem("info", this.state.value);
+  }
+  onChange = event => {
+    this.setState({ value: event.target.value });
+  };
+  render() {
+    const { value } = this.state;
+    return (
+      <div>
+        <input value={value} type="text" onChange={this.onChange} />
+        <p>{value}</p>
+      </div>
+    );
+  }
+}
